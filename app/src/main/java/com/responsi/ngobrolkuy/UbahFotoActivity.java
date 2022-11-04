1470b399962b8409ab4a1c9119bce8311fb5fd3e
Tuneri <galihjunindar@gmail.com>
2022-11-04 16:32:20 +0700

    Nama : Galih Arditiya Yunindar NIM : H1D020091 Shift : G

--- app/src/main/java/com/responsi/ngobrolkuy/UbahFotoActivity.java
+++ app/src/main/java/com/responsi/ngobrolkuy/UbahFotoActivity.java
@@ -10,32 +10,10 @@ import android.widget.TextView;
 
 public class UbahFotoActivity extends AppCompatActivity {
 
-    private ImageView btn_back;
-    private TextView ubahFotonya;
-    private Intent back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ubah_foto);
-
-        btn_back = findViewById(R.id.back_btn);
-        btn_back.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                back = new Intent(UbahFotoActivity.this, SettingActivity.class);
-                startActivity(back);
-            }
-        });
-
-        ubahFotonya = findViewById(R.id.konfirubahfoto);
-        ubahFotonya.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(UbahFotoActivity.this, SettingActivity.class);
-                startActivity(intent);
-            }
-        });
-
     }
 }
